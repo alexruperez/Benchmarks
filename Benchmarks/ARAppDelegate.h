328f332a99425957ffc09ede2a3d299b4d43a850
Alejandro Rupérez <alexruperez@gmail.com>
2014-09-15 09:53:10 +0200

    Initial Commit

---
+++ Benchmarks/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  Benchmarks
+//
+//  Created by Alejandro Rupérez on 15/09/14.
+//  Copyright (c) 2014 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Benchmarks/ARViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARViewController.h
+//  Benchmarks
+//
+//  Created by Alejandro Rupérez on 15/09/14.
+//  Copyright (c) 2014 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARViewController : UIViewController
+
+@end
